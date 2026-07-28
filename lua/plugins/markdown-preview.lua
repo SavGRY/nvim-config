bdf666ff8d1138f4e747b1731f037610780b801f
Rudy Savini <rudy.savini@pytech.it>
2026-07-28 14:29:19 +0200

    feat: add markdown preview

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+return {
+    "ellisonleao/glow.nvim",
+    config = true,
+    cmd = "Glow",
+    -- local changes to the repo!!
+    -- branch = "temp",
+    -- opts = {
+    --     vertical = true,
+    --     autoupdate = true,
+    -- }
+}
